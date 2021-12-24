5ff3e62312b59c10fc7e17167317949834d91757
Mounir Dhahri <soussouheros@gmail.com>
2021-12-24 16:21:17 +0100

    side mission 👹

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react"
 import { Theme, Flex, Button } from "palette"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack"
+import { LogBox } from "react-native"
+
+LogBox.ignoreLogs(["Expected style "])
 
 interface HomeNavigationProps extends NativeStackScreenProps<AppNavigationStack, "Home"> {}
 
 const HomeScreen: React.FC<HomeNavigationProps> = ({ navigation }) => (
   <Theme>
     <Flex flex={1} justifyContent="center" alignItems="center">
-      <Button onPress={navigation.goBack}>Navigate back to Login Screen</Button>
+      <Button onPress={() => navigation.navigate("Login")}>Navigate to Login Screen</Button>
     </Flex>
   </Theme>
 )
@@ -18,7 +21,7 @@ interface LoginNavigationProps extends NativeStackScreenProps<AppNavigationStack
 const LoginScreen: React.FC<LoginNavigationProps> = ({ navigation }) => (
   <Theme>
     <Flex flex={1} justifyContent="center" alignItems="center">
-      <Button onPress={() => navigation.navigate("Home")}>Navigate to Home Screen</Button>
+      <Button onPress={() => navigation.goBack()}>Navigate back to Home Screen</Button>
     </Flex>
   </Theme>
 )
@@ -35,8 +38,8 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
